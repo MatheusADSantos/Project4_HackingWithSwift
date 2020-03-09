94287338372f89e8582836fcf37428e29ba3e383
MatheusADSantos <matheusdevmobile2020@gmail.com>
2020-03-09 10:00:46 -0300

    Creating a right buttonItem to open with an actionSheet, redirecting to a specific website ...

--- Project4_HackingWithSwift/ViewController.swift
+++ Project4_HackingWithSwift/ViewController.swift
@@ -21,11 +21,32 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
         let url = URL(string: "https://www.hackingwithswift.com")!
         webview.load(URLRequest(url: url))
         webview.allowsBackForwardNavigationGestures = true
     }
 
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open Page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
 
+    func openPage(action: UIAlertAction) {
+        guard let actionTitle = action.title else { return }
+        guard let url = URL(string: "https:" + actionTitle) else { return }
+        webview.load(URLRequest(url: url))
+    }
+    //Calling whe the navigation is completed ...
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+        navigationItem.largeTitleDisplayMode = .always
+    }
+    
 }
 
