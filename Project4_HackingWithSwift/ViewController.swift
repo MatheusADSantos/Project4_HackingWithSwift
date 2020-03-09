f3bb073389ae09b614d897be36fd42962389697d
MatheusADSantos <matheusdevmobile2020@gmail.com>
2020-03-09 09:33:10 -0300

    Creating  a webview and relationship with super.view and creating a url request to loadding ...

--- Project4_HackingWithSwift/ViewController.swift
+++ Project4_HackingWithSwift/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webview : WKWebView!
+    
+    override func loadView() {
+        webview = WKWebView()
+        webview.navigationDelegate = self
+        view = webview
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webview.load(URLRequest(url: url))
+        webview.allowsBackForwardNavigationGestures = true
     }
 
 
